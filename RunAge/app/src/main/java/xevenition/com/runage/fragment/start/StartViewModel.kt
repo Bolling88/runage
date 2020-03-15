cedb1f343ba6bcde4b2391ab1aea2166d480f340
Ludwig B <bolling.ludwig@gmail.com>
2020-03-15 12:21:56 +0100

    Fixed so that welcome message is not played if quest is running

--- RunAge/app/src/main/java/xevenition/com/runage/fragment/start/StartViewModel.kt
+++ RunAge/app/src/main/java/xevenition/com/runage/fragment/start/StartViewModel.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import xevenition.com.runage.R
 import xevenition.com.runage.architecture.BaseViewModel
+import xevenition.com.runage.service.EventService
 import xevenition.com.runage.util.AccountUtil
 import xevenition.com.runage.util.FeedbackHandler
 import xevenition.com.runage.util.ResourceUtil
@@ -21,7 +22,9 @@ class StartViewModel(
     init {
         val task = accountUtil.getGamesPlayerInfo()
         task?.addOnSuccessListener {
-            feedbackHandler.speak("${resourceUtil.getString(R.string.runage_welcome_back)} ${it.displayName}")
+            if(!EventService.serviceIsRunning) {
+                feedbackHandler.speak("${resourceUtil.getString(R.string.runage_welcome_back)} ${it.displayName}")
+            }
             liveTextName.postValue(it.displayName)
             observableProfileImage.postValue(it.iconImageUri)
         }
