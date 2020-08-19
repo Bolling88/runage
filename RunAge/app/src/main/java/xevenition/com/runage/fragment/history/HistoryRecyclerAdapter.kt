f5718d1745f14a4da14d1a5a2ce0c78c3efb0a29
Ludwig B <bolling.ludwig@gmail.com>
2020-08-19 09:40:30 +0200

    Native ads looks awesome.

--- RunAge/app/src/main/java/xevenition/com/runage/fragment/history/HistoryRecyclerAdapter.kt
+++ RunAge/app/src/main/java/xevenition/com/runage/fragment/history/HistoryRecyclerAdapter.kt
@@ -183,10 +183,6 @@ class HistoryRecyclerAdapter(
             adView.bodyView = adView.findViewById(R.id.ad_body)
             adView.callToActionView = adView.findViewById(R.id.ad_call_to_action)
             adView.iconView = adView.findViewById(R.id.ad_icon)
-            adView.priceView = adView.findViewById(R.id.ad_price)
-            adView.starRatingView = adView.findViewById(R.id.ad_stars)
-            adView.storeView = adView.findViewById(R.id.ad_store)
-            adView.advertiserView = adView.findViewById(R.id.ad_advertiser)
         }
     }
 
@@ -223,7 +219,6 @@ class HistoryRecyclerAdapter(
         // Some assets are guaranteed to be in every UnifiedNativeAd.
         (adView.headlineView as TextView).text = nativeAd.headline
         (adView.bodyView as TextView).text = nativeAd.body
-        (adView.callToActionView as Button).text = nativeAd.callToAction
 
         // These assets aren't guaranteed to be in every UnifiedNativeAd, so it's important to
         // check before trying to display them.
@@ -234,31 +229,6 @@ class HistoryRecyclerAdapter(
             (adView.iconView as ImageView).setImageDrawable(icon.drawable)
             adView.iconView.visibility = View.VISIBLE
         }
-        if (nativeAd.price == null) {
-            adView.priceView.visibility = View.INVISIBLE
-        } else {
-            adView.priceView.visibility = View.VISIBLE
-            (adView.priceView as TextView).text = nativeAd.price
-        }
-        if (nativeAd.store == null) {
-            adView.storeView.visibility = View.INVISIBLE
-        } else {
-            adView.storeView.visibility = View.VISIBLE
-            (adView.storeView as TextView).text = nativeAd.store
-        }
-        if (nativeAd.starRating == null) {
-            adView.starRatingView.visibility = View.INVISIBLE
-        } else {
-            (adView.starRatingView as RatingBar).rating = nativeAd.starRating.toFloat()
-            adView.starRatingView.setVisibility(View.VISIBLE)
-        }
-        if (nativeAd.advertiser == null) {
-            adView.advertiserView.visibility = View.INVISIBLE
-        } else {
-            (adView.advertiserView as TextView).text = nativeAd.advertiser
-            adView.advertiserView.setVisibility(View.VISIBLE)
-        }
-
         // Assign native ad object to the native view.
         adView.setNativeAd(nativeAd)
     }
